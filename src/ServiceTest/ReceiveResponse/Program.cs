b4e865a20fcae7c57d2ab943b11ceae2f849f7e1
Robert Zhang <louirobert@gmail.com>
2024-02-23 18:34:09 +0800

    Add ServiceTest

---
+++ src/ServiceTest/ReceiveResponse/Program.cs
@@ -0,0 +1,99 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+namespace ReceiveResponse;
+
+class Program
+{
+    const string ENV_CONNECTION_STRING = "STORAGE_CONNECTION_STRING";
+
+    static void ShowUsage()
+    {
+        var usage = @"
+ReceiveResponse -q {queue} [-v]
+";
+        Console.WriteLine(usage);
+    }
+
+    static (string queue, bool verbose) ParseCommandLine()
+    {
+        var args = Environment.GetCommandLineArgs();
+        string? queueName = null;
+        bool verbose = false;
+        try
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if ("-q".Equals(args[i], StringComparison.Ordinal))
+                {
+                    queueName = args[++i];
+                    if (string.IsNullOrEmpty(queueName))
+                    {
+                        throw new ArgumentException($"Parameter '-q <name>' must not be empty!");
+                    }
+                }
+                if ("-v".Equals(args[i], StringComparison.Ordinal))
+                {
+                    verbose = true;
+                }
+                else if ("-h".Equals(args[i], StringComparison.Ordinal))
+                {
+                    ShowUsage();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    throw new ArgumentException($"Unrecognized parameter '{args[i]}'!");
+                }
+            }
+            if (queueName == null)
+            {
+                throw new ArgumentException($"At least a required parameter is missing!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Error when parsing arguments: {ex}");
+            ShowUsage();
+            Environment.Exit(1);
+        }
+        return (queueName, verbose);
+    }
+
+    static int Main(string[] args)
+    {
+        var connectionString = Environment.GetEnvironmentVariable(ENV_CONNECTION_STRING);
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            Console.Error.WriteLine($"Environment variable {ENV_CONNECTION_STRING} is required but missing!");
+            return 1;
+        }
+
+        var (queueName, verbose) = ParseCommandLine();
+
+        Console.WriteLine($"Create queue {queueName} if it doesn't exists.");
+        var client = new QueueClient(connectionString, queueName);
+        client.CreateIfNotExists();
+
+        Console.WriteLine($"Receive message from the queue.");
+
+        var cts = new CancellationTokenSource();
+        var token = cts.Token;
+
+        Console.CancelKeyPress += (sender, args) => {
+            args.Cancel = true;
+            cts.Cancel();
+        };
+
+        while (!token.IsCancellationRequested)
+        {
+            QueueMessage message = client.ReceiveMessage(cancellationToken: token);
+            Console.WriteLine($"Received a message of length {message.MessageText} at {DateTimeOffset.Now}.");
+            if (verbose)
+            {
+                Console.WriteLine(message.MessageText);
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/ServiceTest/SendRequest/Program.cs
@@ -0,0 +1,109 @@
+﻿using Azure.Storage.Queues;
+
+namespace SendRequest;
+
+class Program
+{
+    const string ENV_CONNECTION_STRING = "STORAGE_CONNECTION_STRING";
+
+    static void ShowUsage()
+    {
+        var usage = @"
+SendRequest -c {count} -m {message} -q {queue}
+
+or
+
+SendRequest -c {count} -q {queue} -
+
+A single ""-"" means the message is read from stdin.
+";
+        Console.WriteLine(usage);
+    }
+
+    static (int count, string message, string queue) ParseCommandLine()
+    {
+        var args = Environment.GetCommandLineArgs();
+        int count = 0;
+        string? message = null;
+        string? queueName = null;
+        try
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                if ("-c".Equals(args[i], StringComparison.Ordinal))
+                {
+                    count = int.Parse(args[++i]);
+                    if (count <= 0)
+                    {
+                        throw new ArgumentException($"Parameter '-c <count>' must be greater than 0!");
+                    }
+                }
+                else if ("-m".Equals(args[i], StringComparison.Ordinal))
+                {
+                    message = args[++i];
+                }
+                else if ("-".Equals(args[i], StringComparison.Ordinal))
+                {
+                    using var stdin = Console.OpenStandardInput();
+                    using var reader = new StreamReader(stdin);
+                    message = reader.ReadToEnd();
+                }
+                else if ("-q".Equals(args[i], StringComparison.Ordinal))
+                {
+                    queueName = args[++i];
+                    if (string.IsNullOrEmpty(queueName))
+                    {
+                        throw new ArgumentException($"Parameter '-q <name>' must not be empty!");
+                    }
+                }
+                else if ("-h".Equals(args[i], StringComparison.Ordinal))
+                {
+                    ShowUsage();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    throw new ArgumentException($"Unrecognized parameter '{args[i]}'!");
+                }
+            }
+            if (count == 0 || message == null || queueName == null)
+            {
+                throw new ArgumentException($"At least a required parameter is missing!");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Error when parsing arguments: {ex}");
+            ShowUsage();
+            Environment.Exit(1);
+        }
+        return (count, message, queueName);
+    }
+
+    static int Main(string[] args)
+    {
+        var connectionString = Environment.GetEnvironmentVariable(ENV_CONNECTION_STRING);
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            Console.Error.WriteLine($"Environment variable {ENV_CONNECTION_STRING} is required but missing!");
+            return 1;
+        }
+
+        var (count, message, queueName) = ParseCommandLine();
+
+        Console.WriteLine($"Create queue {queueName} if it doesn't exists.");
+        var client = new QueueClient(connectionString, queueName);
+        client.CreateIfNotExists();
+
+        Console.WriteLine($"Send {count} messages, each of length {message.Length}, to the queue.");
+        var tasks = new Task[count];
+        Console.WriteLine($"Start sending at {DateTimeOffset.Now}");
+        for (int i = 0; i < count; i++)
+        {
+            tasks[i] = client.SendMessageAsync(message);
+        }
+        Task.WaitAll(tasks);
+        Console.WriteLine($"End sending at {DateTimeOffset.Now}");
+        return 0;
+    }
+}
