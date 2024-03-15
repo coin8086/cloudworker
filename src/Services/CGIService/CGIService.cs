2a3a8970108649851fe005a7101b62011ef582cb
Robert Zhang <louirobert@gmail.com>
2024-03-15 12:13:01 +0800

    Refactor services: change root namespaces to "Cloud.Soa.<service>".

--- src/Services/CGIService/CGIService.cs
+++ src/Services/CGIService/CGIService.cs
@@ -9,7 +9,7 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Cloud.Soa;
+namespace Cloud.Soa.CGIService;
 
 public class CGIServiceOptions
 {
--- src/Services/EchoService/EchoService.cs
+++ src/Services/EchoService/EchoService.cs
@@ -1,7 +1,7 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 
-namespace Cloud.Soa;
+namespace Cloud.Soa.EchoService;
 
 public class EchoService : ISoaService
 {
