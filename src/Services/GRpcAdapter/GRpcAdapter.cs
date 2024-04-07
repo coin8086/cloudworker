6c17bdc8b7ab4951ce1fd91216024a9c1e4e24f1
Robert Zhang <louirobert@gmail.com>
2024-04-07 17:20:23 +0800

    Add skeleton code for Services/GRpcAdapter

---
+++ src/Services/GRpcAdapter/GRpcAdapter.cs
@@ -0,0 +1,43 @@
+﻿using CloudWorker.ServiceInterface;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CloudWorker.GRpcAdapter;
+
+public class GRpcAdapterOptions
+{
+    public string? ServerURL {  get; set; }
+}
+
+public class GRpcAdapter : UserService
+{
+    private readonly GRpcAdapterOptions _options;
+
+    private GrpcChannel _channel;
+
+    public GRpcAdapter(ILogger logger, IConfiguration hostConfig) : base(logger, hostConfig)
+    {
+        _options = LoadConfiguration(_logger);
+        _channel = GrpcChannel.ForAddress(_options.ServerURL!);
+    }
+
+    private static GRpcAdapterOptions LoadConfiguration(ILogger logger)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task InitializeAsync(CancellationToken cancel = default)
+    {
+        //TODO: Start gRPC server...
+        throw new NotImplementedException();
+    }
+
+    public override Task<string> InvokeAsync(string input, CancellationToken cancel = default)
+    {
+        //TODO: Can an invoker be a class member?
+        var invoker = _channel.CreateCallInvoker();
+        //invoker.AsyncUnaryCall()
+        throw new NotImplementedException();
+    }
+}
