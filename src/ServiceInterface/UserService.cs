1f090486330d62fdd5a863794aaba087bc55f6bf
Robert Zhang <louirobert@gmail.com>
2024-04-08 22:11:43 +0800

    Do not load seetings file when file name is empty.

--- src/ServiceInterface/UserService.cs
+++ src/ServiceInterface/UserService.cs
@@ -8,9 +8,9 @@ public abstract class UserService<TOptions> : UserServiceBase where TOptions : c
 {
     protected TOptions? _options;
 
-    protected virtual string _settingsFileName { get; } = "ServiceSettings.json";
+    protected virtual string _settingsFileName { get; } = string.Empty;
 
-    protected virtual string _environmentPrefix { get; } = "";
+    protected virtual string _environmentPrefix { get; } = string.Empty;
 
     public UserService(ILogger logger, IConfiguration hostConfig) : base(logger, hostConfig)
     {
@@ -19,18 +19,22 @@ public abstract class UserService<TOptions> : UserServiceBase where TOptions : c
 
     protected virtual void LoadConfiguration()
     {
-        var configFileBase = Path.GetDirectoryName(this.GetType().Assembly.Location);
-        if (string.IsNullOrWhiteSpace(configFileBase))
+        var builder = new ConfigurationBuilder();
+
+        if (!string.IsNullOrEmpty(_settingsFileName))
         {
-            configFileBase = Directory.GetCurrentDirectory();
+            var configFileBase = Path.GetDirectoryName(this.GetType().Assembly.Location);
+            if (string.IsNullOrWhiteSpace(configFileBase))
+            {
+                configFileBase = Directory.GetCurrentDirectory();
+            }
+
+            _logger.LogInformation("Look up configuration file in '{configFileBase}'.", configFileBase);
+
+            builder.SetBasePath(configFileBase).AddJsonFile(_settingsFileName, true);
         }
 
-        _logger.LogInformation("Look up configuration file in '{configFileBase}'.", configFileBase);
-
-        var builder = new ConfigurationBuilder()
-            .SetBasePath(configFileBase)
-            .AddJsonFile(_settingsFileName, true)
-            .AddEnvironmentVariables(_environmentPrefix);
+        builder.AddEnvironmentVariables(_environmentPrefix);
 
         var configuration = builder.Build();
         _options = configuration.Get<TOptions>();
