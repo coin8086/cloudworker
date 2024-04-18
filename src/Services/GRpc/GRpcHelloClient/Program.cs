a7c4629b2e02b43502c3bf0a46ed39cb5ff86a64
Robert Zhang <louirobert@gmail.com>
2024-04-18 18:55:38 +0800

    Implemented GRpcHelloClient

--- src/Services/GRpc/GRpcHelloClient/Program.cs
+++ src/Services/GRpc/GRpcHelloClient/Program.cs
@@ -1,9 +1,72 @@
-﻿namespace GRpcHelloClient;
+﻿using CloudWorker.E2E;
+using CloudWorker.MessageQueue;
+using CloudWorker.Services.GRpcAdapter.Client;
+using CommandLine;
+using GRpcHello;
+
+namespace GRpcHelloClient;
 
 class Program
 {
-    static void Main(string[] args)
+    class Options : CloudWorker.E2E.QueueOptions
     {
-        Console.WriteLine("Hello, World!");
+        [Option(Hidden = true, Required = false)]
+        public override string? QueueName { get; set; }
+
+        [Option('S', "request-queue", Default = (string)"requests")]
+        public string? RequestQueueName { get; set; }
+
+        [Option('R', "response-queue", Default = (string)"responses")]
+        public string? ResponseQueueName { get; set; }
+
+        public override void Validate()
+        {
+            base.Validate();
+            if (string.IsNullOrWhiteSpace(RequestQueueName))
+            {
+                throw new ArgumentException("RequestQueueName cannot be empty!");
+            }
+            if (string.IsNullOrWhiteSpace(ResponseQueueName))
+            {
+                throw new ArgumentException("ResponseQueueName cannot be empty!");
+            }
+        }
+    }
+
+    static async Task<int> Main(string[] args)
+    {
+        return await Parser.Default.ParseArguments<Options>(args)
+            .MapResult(RunAsync, _ => Task.FromResult(1)).ConfigureAwait(false);
+    }
+
+    static IMessageQueue CreateSender(Options options)
+    {
+        var queueOpts = new CloudWorker.E2E.QueueOptions(options) { QueueName = options.RequestQueueName };
+        return QueueClient.Create(queueOpts);
+    }
+
+    static IMessageQueue CreateReceiver(Options options)
+    {
+        var queueOpts = new CloudWorker.E2E.QueueOptions(options) { QueueName = options.ResponseQueueName };
+        return QueueClient.Create(queueOpts);
+    }
+
+    static async Task<int> RunAsync(Options options)
+    {
+        options.Validate();
+
+        var sender = CreateSender(options);
+        var gMethod = Greeter.Descriptor.FindMethodByName("SayHello");
+        var gMsg = new HelloRequest() { Name = "Rob" };
+        var request = new Request(gMethod, gMsg);
+
+        Console.WriteLine($"Sending '{gMsg}' to '{gMethod.FullName}'");
+        await sender.SendGRpcMessageAsync(request);
+
+        var receiver = CreateReceiver(options);
+        var response = await receiver.WaitGRpcMessageAsync<HelloReply>();
+        Console.WriteLine($"Received {response.GRpcMessage}");
+
+        return 0;
     }
 }
