e58d6fa6ca57bc2db8df3f77d7db570b154009e8
Robert Zhang <louirobert@gmail.com>
2024-04-25 17:47:40 +0800

    Fix an issue of HTTP2 without TLS in GRpcHello

--- src/Services/GRpc/GRpcHello/Program.cs
+++ src/Services/GRpc/GRpcHello/Program.cs
@@ -1,4 +1,6 @@
 using GRpcHello.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
 
 namespace GRpcHello;
 
@@ -7,6 +9,26 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        //NOTE: gRPC requires HTTP2 and for HTTP2 without TLS in ASP.NET, see
+        //https://learn.microsoft.com/en-us/aspnet/core/grpc/aspnetcore?view=aspnetcore-8.0&tabs=visual-studio#protocol-negotiation
+        //Also note the configuration like the following doesn't work for http2 without TLS (on Linux)!
+        //"Kestrel": {
+        //    "Endpoints": {
+        //        "http": {
+        //            "Url": "http://localhost:8080",
+        //            "Protocols": "Http2"
+        //        }
+        //    }
+        //}
+        //So code configuration is made here.
+        builder.WebHost.ConfigureKestrel((context, serverOptions) =>
+        {
+            serverOptions.Listen(IPAddress.Loopback, 8080, listenOptions =>
+            {
+                listenOptions.Protocols = HttpProtocols.Http2;
+            });
+        });
 
         // Add services to the container.
         builder.Services.AddGrpc();
