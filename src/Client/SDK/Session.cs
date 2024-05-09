962222a9d9cd51d55361851d6a79ec62cd278462
Robert Zhang <louirobert@gmail.com>
2024-05-09 16:24:01 +0800

    Complete implementation of Client/SDK/Session.

--- src/Client/SDK/Session.cs
+++ src/Client/SDK/Session.cs
@@ -2,6 +2,7 @@
 using CloudWorker.MessageQueue;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -24,19 +25,53 @@ public class Session : ISession
 
     private ICluster _cluster;
 
-    private Session(ICluster cluster)
+    private ClusterProperties? _properties;
+
+    private ILoggerFactory? _loggerFactory;
+
+    private Session(ICluster cluster, ILoggerFactory? loggerFactory = null)
     {
         _cluster = cluster;
+        _loggerFactory = loggerFactory;
+    }
+
+    private async Task GetClusterPropertiesAsync()
+    {
+        _properties = await _cluster.GetPropertiesAsync();
+        _properties.Validate();
+    }
+
+    //TODO: Support Storage queue
+    private IMessageQueue CreateQueueClient(bool sender)
+    {
+        Debug.Assert(_properties != null);
+
+        var queueType = _properties.QueueProperties?.QueueType;
+        if (!ServiceBusQueue.QueueType.Equals(queueType))
+        {
+            throw new NotImplementedException($"Queue type {queueType} is not supported.");
+        }
+
+        var queueOptions = new ServiceBusQueueOptions()
+        {
+            QueueType = queueType,
+            ConnectionString = _properties.QueueProperties!.ConnectionString,
+            QueueName = sender ? _properties.QueueProperties.RequestQueueName : _properties.QueueProperties.ResponseQueueName,
+            MessageLease = 60, //TODO: Make it configurable in ClusterConfig
+            RetryOnThrottled = true
+        };
+        var logger = _loggerFactory?.CreateLogger<ServiceBusQueue>();
+        return new ServiceBusQueue(queueOptions, logger);
     }
 
     public IMessageQueue CreateSender()
     {
-        throw new NotImplementedException();
+        return CreateQueueClient(true);
     }
 
     public IMessageQueue CreateReceiver()
     {
-        throw new NotImplementedException();
+        return CreateQueueClient(false);
     }
 
     public static async Task<Session> CreateOrUpdateAsync(TokenCredential credential, SessionConfig sessionConfig, string? sessionId = null,
@@ -45,7 +80,9 @@ public class Session : ISession
         var logger = loggerFactory?.CreateLogger<Cluster>();
         var cluster = new Cluster(credential, sessionConfig, sessionId, logger);
         await cluster.CreateOrUpdateAsync(token);
-        return new Session(cluster);
+        var session = new Session(cluster, loggerFactory);
+        await session.GetClusterPropertiesAsync();
+        return session;
     }
 
     public static async Task<Session> GetAsync(TokenCredential credential, string sessionId,
@@ -54,7 +91,9 @@ public class Session : ISession
         var logger = loggerFactory?.CreateLogger<Cluster>();
         var cluster = new Cluster(credential, sessionId, logger);
         await cluster.ValidateAsync(token);
-        return new Session(cluster);
+        var session = new Session(cluster, loggerFactory);
+        await session.GetClusterPropertiesAsync();
+        return session;
     }
 
     public static async Task DestroyAsync(TokenCredential credential, string sessionId,
