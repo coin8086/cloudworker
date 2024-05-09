21050df31f451da87bb450d801a4af14380d3d32
Robert Zhang <louirobert@gmail.com>
2024-05-09 15:57:59 +0800

    Add ValidateObjectAttribute and refactor IValidatable.Validate

--- src/Client/SDK/IValidatable.cs
+++ src/Client/SDK/IValidatable.cs
@@ -6,13 +6,34 @@ namespace CloudWorker.Client.SDK;
 [AttributeUsage(AttributeTargets.Property)]
 public class RequiredAttribute : Attribute
 {
-    public RequiredAttribute() {}
+    public RequiredAttribute() { }
 }
 
 [AttributeUsage(AttributeTargets.Property)]
 public class ValidateCollectionAttribute : Attribute
 {
     public ValidateCollectionAttribute() { }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class ValidateObjectAttribute : Attribute
+{
+    public ValidateObjectAttribute() { }
+}
+
+public class ValidationError : ApplicationException
+{
+    public string Error { get; }
+
+    public ValidationError(string error)
+    {
+        Error = error;
+    }
+
+    public override string ToString()
+    {
+        return $"ValidationError: {Error}";
+    }
 }
 
 public interface IValidatable
@@ -26,32 +47,40 @@ public interface IValidatable
         var properties = type.GetProperties();
         foreach (var property in properties)
         {
-            if (typeof(string).IsAssignableFrom(property.PropertyType))
+            var attr = property.GetCustomAttributes(typeof(RequiredAttribute), true);
+            if (attr.Length > 0)
             {
-                var attr = property.GetCustomAttributes(typeof(RequiredAttribute), true);
-                if (attr.Length > 0)
+                var value = property.GetValue(obj);
+                if (value != null)
                 {
-                    var value = (string?)property.GetValue(obj);
-                    if (string.IsNullOrWhiteSpace(value))
-                    {
-                        throw new ArgumentException("The property cannot be empty.", property.Name);
-                    }
+                    throw new ValidationError($"The property {property.Name} cannot be null.");
+                }
+                if (value is string str && string.IsNullOrEmpty(str))
+                {
+                    throw new ValidationError($"The property {property.Name} cannot be empty.");
                 }
             }
-            else if (typeof(IEnumerable<IValidatable>).IsAssignableFrom(property.DeclaringType))
+
+            attr = property.GetCustomAttributes(typeof(ValidateCollectionAttribute), true);
+            if (attr.Length > 0)
             {
                 var collection = (IEnumerable<IValidatable>?)property.GetValue(obj);
-                if (collection == null)
-                {
-                    continue;
-                }
-                var attr = property.GetCustomAttributes(typeof(ValidateCollectionAttribute), true);
-                if (attr.Length > 0)
+                if (collection != null)
                 {
                     foreach (var value in collection)
                     {
                         value.Validate();
                     }
+                }
+            }
+
+            attr = property.GetCustomAttributes(typeof(ValidateObjectAttribute), true);
+            if (attr.Length > 0)
+            {
+                var validatable = (IValidatable?)property.GetValue(obj);
+                if (validatable != null)
+                {
+                    validatable.Validate();
                 }
             }
         }
